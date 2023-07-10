ab7d9fefcb58d158386cbf6734f5becf80bbf6d3
kesavan <kesavan@mericaninc.com>
2023-07-10 17:37:14 +0530

    store formdatas to firestore and store the uploaded image file to storage bucket in firestore

--- components/form/form.jsx
+++ components/form/form.jsx
@@ -2,6 +2,10 @@
 import { useState } from "react";
 import InputFile from "../input-file/inputfile";
 import UserTag from "../user-tag/usertag";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { app } from "@/firebase/firebase.config";
+import { useSession } from "next-auth/react";
+import { doc, setDoc, getFirestore } from "firebase/firestore";
 
 const Form = () => {
   const [title, setTitle] = useState();
@@ -9,11 +13,54 @@ const Form = () => {
   const [link, setLink] = useState();
   const [file, setFile] = useState();
 
-  const handleSubmit = () => {};
+  const { data: session } = useSession();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, desc, link, file);
+    uploadFile();
+  };
+
+  const uploadFile = async () => {
+    const postId = Date.now();
+
+    // Initialize Cloud Storage and get a reference to the service
+    const storage = getStorage(app);
+    const db = getFirestore(app);
+    // Create a reference to 'mountains.jpg'
+    const imageRef = ref(storage, "pinterest/" + file.name);
+    // 'file' comes from the Blob or File API
+    await uploadBytes(imageRef, file)
+      .then((snapshot) => {
+        console.log("Uploaded a blob or file!");
+      })
+      .then(
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        await getDownloadURL(imageRef).then(async (downloadURL) => {
+          console.log("File available at", downloadURL);
+          const postData = {
+            title,
+            desc,
+            link,
+            image: downloadURL,
+            userName: session.user.name,
+            userEmail: session.user.email,
+            userImage: session.user.image,
+          };
+          await setDoc(doc(db, "pinterest-data", postId), postData).then(
+            (res) => {
+              console.log("saved");
+            }
+          );
+        })
+      );
+  };
+
   return (
     <form
       className="flex flex-col justify-center items-center mt-10"
-      onSubmit={() => handleSubmit()}
+      onSubmit={(e) => handleSubmit(e)}
     >
       <div className="form-control w-full max-w-xs">
         <label className="label">
--- components/input-file/inputfile.jsx
+++ components/input-file/inputfile.jsx
@@ -1,8 +1,9 @@
-const InputFile = () => {
+const InputFile = ({ setFile }) => {
   return (
     <input
       type="file"
       className="file-input file-input-bordered file-input-md w-full max-w-xs"
+      onChange={(e) => setFile(e.target.files[0])}
     />
   );
 };
