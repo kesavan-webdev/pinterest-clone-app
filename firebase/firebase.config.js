db492a657b646491af099171696f7980075b7ca5
kesavan <kesavan@mericaninc.com>
2023-07-10 11:59:59 +0530

    install firebase

---
+++ firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxHaa7TcTO-eRo5FP0hVMO5fGmmDD3KzM",
+  authDomain: "pinterest-clone-app-2d7d6.firebaseapp.com",
+  projectId: "pinterest-clone-app-2d7d6",
+  storageBucket: "pinterest-clone-app-2d7d6.appspot.com",
+  messagingSenderId: "34018055255",
+  appId: "1:34018055255:web:321b50f55318ee6a8aff12",
+};
+
+// Initialize Firebase
+export default app = initializeApp(firebaseConfig);
