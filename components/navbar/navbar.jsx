254f1b46f22e3cd71866e20a76aae2a53a3deb4a
kesavan <kesavan@mericaninc.com>
2023-07-10 11:43:26 +0530

    add  navbar and install daisyUi

---
+++ components/navbar/navbar.jsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    // <nav>
+
+    //
+    //   <div>
+    //     <input className="" type="search" />
+    //   </div>
+    // </nav>
+    <div className="navbar bg-base-100">
+      <div>
+        <Image
+          src="/pinterest-logo-emblem-png-11.png"
+          width={50}
+          height={50}
+          alt="logo"
+        />
+      </div>
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">Pinterest</a>
+      </div>
+      <div>
+        <button className="bg-black text-white p-3 rounded-full">Home</button>
+      </div>
+      <div>
+        <button className="bg-white p-3 rounded-full">Create</button>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
+        </div>
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <Image src="" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
