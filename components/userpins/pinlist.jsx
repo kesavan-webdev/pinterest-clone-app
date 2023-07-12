4ba9257af15155668639757a7c31efbb29821442
kesavan <kesavan@mericaninc.com>
2023-07-12 12:24:27 +0530

    add styles

--- components/userpins/pinlist.jsx
+++ components/userpins/pinlist.jsx
@@ -2,15 +2,17 @@ import PinItem from "../pin-item/pinitem";
 
 const PinList = ({ listOfPins }) => {
   console.log(listOfPins);
-  return listOfPins.map((value, index) => {
-    return (
-      <>
-        <div className="mt-7 px-2 md:px-5 columns-2 md:columns-3 lg-columns-4 xl-columns-5 space-y-6 mx-auto">
-          <PinItem value={value} key={index} />
-        </div>
-      </>
-    );
-  });
+  return (
+    <div className="mt-7 px-2 md:px-5 columns-2 md:columns-3 lg-col-span-4 xl-col-span-5 space-y-6 mx-auto">
+      {listOfPins.map((value, index) => {
+        return (
+          <div key={index}>
+            <PinItem value={value} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default PinList;
